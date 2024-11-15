677a34398aaa71b15ed8c361e3b51a29d5ab6865
Thanos <atmichai@physics.auth.gr>
2024-11-15 12:52:29 +0200

    completed SecoCollab's visuals

--- src/screens lls/SecoCollab.js
+++ src/screens lls/SecoCollab.js
@@ -12,13 +12,13 @@ import { cardFooter } from "../utils/card-footer.js";
 const SecoCollab = () => {
 	const customDate = useMemo(() => getCustomDateTime(2024, 9), []);
 	// Memoize the date calculations and fetchConfigs to reduce re-calculations
-	const { currentDate, formattedBeginningOfDay } = useMemo(() => calculateDates(customDate), [customDate]);
-	console.log("Current Date", currentDate);
-	console.log("Formatted Beginning of Day", formattedBeginningOfDay);
+	const { currentDate, formattedBeginningOfMonth, formattedBeginningOfDay } =	useMemo(
+		() => calculateDates(customDate), [customDate],
+	);
 
 	const fetchConfigs = useMemo(
-		() => secoConfigs(formattedBeginningOfDay, currentDate),
-		[formattedBeginningOfDay, currentDate],
+		() => secoConfigs(formattedBeginningOfMonth, formattedBeginningOfDay, currentDate),
+		[formattedBeginningOfMonth, formattedBeginningOfDay, currentDate],
 	);
 	const { state } = useInit(organization, fetchConfigs);
 
@@ -57,59 +57,6 @@ const SecoCollab = () => {
 
 	return (
 		<Grid container display="flex" direction="row" justifyContent="space-around" spacing={2}>
-			<Grid item xs={12} md={12} alignItems="center" flexDirection="column" mt={2}>
-				<Card title="Overview" footer={cardFooter({ minutesAgo: state.minutesAgo })}>
-					{[
-						{
-							data: [
-								{
-									x: state.dataSets.overview ? state.dataSets.overview.map((item) => item.timestamp) : [],
-									y: state.dataSets.overview ? state.dataSets.overview.map((item) => item.m_temp01) : [],
-									type: "scatter",
-									mode: "lines",
-									color: "gold",
-									title: "Temperature",
-								},
-							],
-							yaxis: { title: "Temperature (°C)" },
-						},
-						{
-							data: [
-								{
-									x: state.dataSets.overview ? state.dataSets.overview.map((item) => item.timestamp) : [],
-									y: state.dataSets.overview ? state.dataSets.overview.map((item) => item.m_hum01) : [],
-									type: "scatter",
-									mode: "lines",
-									color: "secondary",
-									title: "Humidity",
-								},
-							],
-							yaxis: { title: "Humidity (%)" },
-						},
-						{
-							data: [
-								{
-									x: state.dataSets.overview ? state.dataSets.overview.map((item) => item.timestamp) : [],
-									y: state.dataSets.overview ? state.dataSets.overview.map((item) => item.a_co2) : [],
-									type: "scatter",
-									mode: "markers",
-									color: "third",
-									title: "Co2",
-								},
-							],
-							yaxis: { title: "Co2" },
-						},
-					].map((plotData, index) => (
-						<Plot
-							key={index}
-							scrollZoom
-							data={plotData.data}
-							displayBar={false}
-							yaxis={plotData.yaxis}
-						/>
-					))}
-				</Card>
-			</Grid>
 			<Grid item xs={12} md={12} alignItems="center" flexDirection="row" mt={2}>
 				<Card title="Today's Overview" footer={cardFooter({ minutesAgo: state.minutesAgo })}>
 					<Grid container display="flex" direction="row" justifyContent="space-around" spacing={2}>
@@ -121,7 +68,7 @@ const SecoCollab = () => {
 								value: state.dataSets.todayTemperature && state.dataSets.todayTemperature.length > 0
 									? state.dataSets.todayTemperature.at(-1).avg_m_temp01
 									: null,
-								color: "gold",
+								color: "goldenrod",
 								symbol: "°C",
 							},
 							{
@@ -131,7 +78,7 @@ const SecoCollab = () => {
 								value: state.dataSets.todayHumidity && state.dataSets.todayHumidity.length > 0
 									? state.dataSets.todayHumidity.at(-1).avg_m_hum01
 									: null,
-								color: "secondary",
+								color: "third",
 								symbol: "%",
 							},
 							{
@@ -142,7 +89,7 @@ const SecoCollab = () => {
 									? state.dataSets.todayCo2.at(-1).avg_a_co2
 									: null,
 
-								color: "third",
+								color: "secondary",
 								symbol: "",
 							},
 						].map((plot, index) => (
@@ -169,6 +116,149 @@ const SecoCollab = () => {
 							</Grid>
 						))}
 					</Grid>
+				</Card>
+			</Grid>
+			<Grid item xs={12} md={12} alignItems="center" flexDirection="column" mt={2}>
+				<Card title="Month's Overview" footer={cardFooter({ minutesAgo: state.minutesAgo })}>
+					<Grid container display="flex" direction="row" justifyContent="space-around" spacing={2}>
+						{[
+							{
+								data: [
+									{
+										x: state.dataSets.monthMaxTemperature
+											? state.dataSets.monthMaxTemperature.map((item) => item.interval_start) : [],
+										y: state.dataSets.monthMaxTemperature
+											? state.dataSets.monthMaxTemperature.map((item) => item.max_m_temp01) : [],
+										type: "bar",
+										color: "goldenrod",
+										title: "maxTemperature",
+									},
+									{
+										x: state.dataSets.monthMaxTemperature
+											? state.dataSets.monthMaxTemperature.map((item) => item.interval_start) : [],
+										y: state.dataSets.monthMinTemperature
+											? state.dataSets.monthMinTemperature.map((item) => item.min_m_temp01) : [],
+										type: "bar",
+										color: "gold",
+										title: "minTemperature",
+									},
+								],
+								yaxis: { title: "Temperature (°C)" },
+							},
+							{
+
+								data: [
+									{
+										x: state.dataSets.monthMaxHumidity
+											? state.dataSets.monthMaxHumidity.map((item) => item.interval_start) : [],
+										y: state.dataSets.monthMaxHumidity
+											? state.dataSets.monthMaxHumidity.map((item) => item.max_m_hum01) : [],
+										type: "bar",
+										color: "primary",
+										title: "maxHumidity",
+									},
+									{
+										x: state.dataSets.monthMaxHumidity
+											? state.dataSets.monthMaxHumidity.map((item) => item.interval_start) : [],
+										y: state.dataSets.monthMinHumidity
+											? state.dataSets.monthMinHumidity.map((item) => item.min_m_hum01) : [],
+										type: "bar",
+										color: "third",
+										title: "minHumidity",
+									},
+								],
+								yaxis: { title: "Humidity (%)" },
+							},
+							{
+
+								data: [
+									{
+										x: state.dataSets.monthMaxCo2
+											? state.dataSets.monthMaxCo2.map((item) => item.interval_start) : [],
+										y: state.dataSets.monthMaxCo2
+											? state.dataSets.monthMaxCo2.map((item) => item.max_a_co2) : [],
+										type: "bar",
+										color: "green",
+										title: "maxCo2",
+									},
+									{
+										x: state.dataSets.monthMaxCo2
+											? state.dataSets.monthMaxCo2.map((item) => item.interval_start) : [],
+										y: state.dataSets.monthMinCo2
+											? state.dataSets.monthMinCo2.map((item) => item.min_a_co2) : [],
+										type: "bar",
+										color: "secondary",
+										title: "minCo2",
+									},
+								],
+								yaxis: { title: "Co2" },
+							},
+						].map((plot, index) => (
+							<Grid key={index} item xs={12} md={4} justifyContent="center">
+								<Plot
+									key={index}
+									scrollZoom
+									showLegend={false}
+									data={plot.data}
+									displayBar={false}
+									yaxis={plot.yaxis}
+								/>
+							</Grid>
+						))}
+					</Grid>
+				</Card>
+			</Grid>
+			<Grid item xs={12} md={12} alignItems="center" flexDirection="column" mt={2}>
+				<Card title="Timeline's Overview" footer={cardFooter({ minutesAgo: state.minutesAgo })}>
+					{[
+						{
+							data: [
+								{
+									x: state.dataSets.overview ? state.dataSets.overview.map((item) => item.timestamp) : [],
+									y: state.dataSets.overview ? state.dataSets.overview.map((item) => item.m_temp01) : [],
+									type: "scatter",
+									mode: "lines",
+									color: "goldenrod",
+									title: "Temperature",
+								},
+							],
+							yaxis: { title: "Temperature (°C)" },
+						},
+						{
+							data: [
+								{
+									x: state.dataSets.overview ? state.dataSets.overview.map((item) => item.timestamp) : [],
+									y: state.dataSets.overview ? state.dataSets.overview.map((item) => item.m_hum01) : [],
+									type: "scatter",
+									mode: "lines",
+									color: "third",
+									title: "Humidity",
+								},
+							],
+							yaxis: { title: "Humidity (%)" },
+						},
+						{
+							data: [
+								{
+									x: state.dataSets.overview ? state.dataSets.overview.map((item) => item.timestamp) : [],
+									y: state.dataSets.overview ? state.dataSets.overview.map((item) => item.a_co2) : [],
+									type: "scatter",
+									mode: "markers",
+									color: "secondary",
+									title: "Co2",
+								},
+							],
+							yaxis: { title: "Co2" },
+						},
+					].map((plotData, index) => (
+						<Plot
+							key={index}
+							scrollZoom
+							data={plotData.data}
+							displayBar={false}
+							yaxis={plotData.yaxis}
+						/>
+					))}
 				</Card>
 			</Grid>
 		</Grid>
