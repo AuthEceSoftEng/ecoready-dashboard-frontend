94b481cf6dca96d94b6e4a1532553a811a82feb5
Thanos <atmichai@physics.auth.gr>
2025-01-23 13:26:42 +0200

    added missing LLs' screens, with "under construction" indication

--- src/utils/useful-constants.js
+++ src/utils/useful-constants.js
@@ -14,15 +14,15 @@ export const monthNames = [
 ];
 
 export const labs = [
-	{ title: "AgroLab", path: "../agrolab" },
-	{ title: "HiveLab", path: "../hivelab" },
-	// { title: "AIDEMEC", path: "../aidemec", region: "Mediterranean", coordinates: [40.575_348, 15.882_535], products: ["barley", "olive","wheat", "tomato", "beans"] },
-	// { title: "CONCATLL", path: "../concatll", region: "Mediterranean", coordinates: [41.586_168, 1.514_272], products: ["apple", "olive", "wheat", "poultry", "fish"] },
+	// { title: "AgroLab", path: "../agrolab" },
+	// { title: "HiveLab", path: "../hivelab" },
+	{ title: "AIDEMEC", path: "../aidemec", region: "Mediterranean", coordinates: [40.575_348, 15.882_535], products: ["barley", "olive", "wheat", "tomato", "beans"] },
+	{ title: "CONCATLL", path: "../concatll", region: "Mediterranean", coordinates: [41.586_168, 1.514_272], products: ["apple", "olive", "wheat", "poultry", "fish"] },
 	{ title: "EcoReadyMasuria", path: "../ecoreadymasuria", region: "Central Europe", coordinates: [53.619_53, 20.366_513], products: ["barley", "maize", "sunflower", "red clover", "milk"] },
 	{ title: "EcoVitaLL", path: "../ecovitall", region: "Central Europe", coordinates: [47.492_367, 19.044_356], products: ["vertival farm"] },
 	{ title: "Esappin", path: "../esappin", region: "Western Europe", coordinates: [51.443_657, 7.657_856], products: ["barley", "oats", "raspberry", "rapeseed", "mushroom"] },
 	{ title: "LivOrganic", path: "../livorganic", region: "Scandinavia", products: ["barley", "potato"] },
-	// { title: "LOFS", path: "../lofs", region: "Western Europe", coordinates: { cite1: [47.710_385, 1.727_322], cite2: [47.769_127, -0.327_92] }, , products: ["mushroom", "grass"] },
+	{ title: "LOFS", path: "../lofs", region: "Western Europe", coordinates: { cite1: [47.710_385, 1.727_322], cite2: [47.769_127, -0.327_92] }, products: ["mushroom", "grass"] },
 	{ title: "Probio", path: "../probio", region: "Central Europe", coordinates: [49.039_97, 16.864_949], products: ["apple", "olive", "wheat", "poultry", "fish"] },
 	{ title: "Seco Collab", path: "../secocollab", region: "Scandinavia", products: ["vertical farm"] },
 	{ title: "THALLA", path: "../thalla", region: "Mediterranean", coordinates: [38.533_333, 22.366_667], products: ["honey", "olive", "wheat", "tomato", "fish"] },
