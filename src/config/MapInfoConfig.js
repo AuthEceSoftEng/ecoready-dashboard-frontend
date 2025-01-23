83493803b9eb10f158fdf8a33d1ce051c7027bed
thdiaman <themisdiamantopoulos@hotmail.com>
2025-01-23 23:10:30 +0200

    Add example products for different map layers

--- src/config/MapInfoConfig.js
+++ src/config/MapInfoConfig.js
@@ -35,6 +35,44 @@ export const mapInfoConfigs = (country, product, year) => {
 			},
 		];
 	}
+	else if (product === "wine") {
+		return [
+			{
+				type: "stats",
+				project: "wine",
+				collection: "__prices__",
+				params: JSON.stringify({ attribute: ["price"], stat: "avg", interval: `every_12_days`, start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
+				attributename: "avg_price",
+				metric: "Average Price",
+				unit: "€/HL.",
+				plotId: "productPrices",
+			},
+		];
+	}
+	else if (product === "olive_oil") {
+		return [
+			{
+				type: "stats",
+				project: "olive_oil",
+				collection: "__prices__",
+				params: JSON.stringify({ attribute: ["price"], stat: "avg", interval: `every_12_days`, start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
+				attributename: "avg_price",
+				metric: "Average Price",
+				unit: "€/100kg",
+				plotId: "productPrices",
+			},
+			{
+				type: "stats",
+				project: "olive_oil",
+				collection: "olive_oil_annual_production",
+				params: JSON.stringify({ attribute: ["year_production_quantity"], stat: "sum", interval: "every_12_months", start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
+				attributename: "sum_year_production_quantity",
+				metric: "Production",
+				unit: "t",
+				plotId: "productProduction1",
+			},
+		];
+	}
 	else if (product === "sugar") {
 		return [
 			{
@@ -42,6 +80,7 @@ export const mapInfoConfigs = (country, product, year) => {
 				project: "sugar",
 				collection: "__prices__",
 				params: JSON.stringify({ attribute: ["price"], stat: "avg", interval: `every_12_days`, start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
+				attributename: "avg_price",
 				metric: "Average Price",
 				unit: "€/t",
 				plotId: "productPrices",
@@ -51,6 +90,7 @@ export const mapInfoConfigs = (country, product, year) => {
 				project: "sugar",
 				collection: "__production__",
 				params: JSON.stringify({ attribute: ["gross_production"], stat: "sum", interval: "every_12_months", start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
+				attributename: "sum_gross_production",
 				metric: "Gross Production",
 				unit: "t",
 				plotId: "productProduction1",
