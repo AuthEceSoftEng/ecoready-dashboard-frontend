a466f91bc2c77cbda226d2a79f365dbc5e95a4ae
thdiaman <themisdiamantopoulos@hotmail.com>
2025-01-25 19:37:29 +0200

    Update rice collections

--- src/config/MapInfoConfig.js
+++ src/config/MapInfoConfig.js
@@ -6,7 +6,7 @@ export const mapInfoConfigs = (country, product, year) => {
 			{
 				type: "stats",
 				project: "rice",
-				collection: "__rice_prices__",
+				collection: "__prices__",
 				params: JSON.stringify({ attribute: ["price"], stat: "avg", interval: `every_12_days`, start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
 				attributename: "avg_price",
 				metric: "Average Price",
@@ -16,7 +16,7 @@ export const mapInfoConfigs = (country, product, year) => {
 			{
 				type: "stats",
 				project: "rice",
-				collection: "__rice_production__",
+				collection: "__production__",
 				params: JSON.stringify({ attribute: ["milled_rice_equivalent_quantity"], stat: "sum", interval: "every_12_months", start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
 				attributename: "sum_milled_rice_equivalent_quantity",
 				metric: "Milled Rice Quantity",
@@ -26,7 +26,7 @@ export const mapInfoConfigs = (country, product, year) => {
 			{
 				type: "stats",
 				project: "rice",
-				collection: "__rice_production__",
+				collection: "__production__",
 				params: JSON.stringify({ attribute: ["rice_husk_quantity"], stat: "sum", interval: "every_12_months", start_time: `${year}-01-01`, end_time: `${year}-12-31`, group_by: "key", }),
 				attributename: "sum_rice_husk_quantity",
 				metric: "Rice Husk Quantity",
