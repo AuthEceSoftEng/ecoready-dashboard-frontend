0c2abec66ba309e9ac30c41a3d01d86c8b093540
Thanos <atmichai@physics.auth.gr>
2025-09-16 15:43:08 +0300

    Modification of Efsa calls

--- src/config/EfsaConfig.js
+++ src/config/EfsaConfig.js
@@ -3,41 +3,41 @@ export const organization = "efsa";
 const efsaConfigs = (country, year = null) => {
 	// Always include the main data config
 	const configs = [
-		{
-			type: "data",
-			project: "efsaproject",
-			collection: "efsa_data",
-			params: JSON.stringify({
-				attributes: ["key", "param", "resval", "resunit", "resloq"],
-				filters: [
-					{
-						property_name: "resval",
-						operator: "gt",
-						property_value: 0,
-					},
-					// Conditionally include country filter
-					...(country ? [{
-						property_name: "sampcountry",
-						operator: "eq",
-						property_value: country,
-					}] : []),
-					// Conditionally include year filters
-					...(year ? [
-						{
-							property_name: "timestamp",
-							operator: "gte",
-							property_value: `${year}-01-01`,
-						},
-						{
-							property_name: "timestamp",
-							operator: "lte",
-							property_value: `${year}-12-31`,
-						},
-					] : []),
-				],
-			}),
-			plotId: "metrics",
-		},
+		// {
+		// 	type: "data",
+		// 	project: "efsaproject",
+		// 	collection: "efsa_data",
+		// 	params: JSON.stringify({
+		// 		attributes: ["key", "param", "resval", "resunit", "resloq"],
+		// 		filters: [
+		// 			{
+		// 				property_name: "resval",
+		// 				operator: "gt",
+		// 				property_value: 0,
+		// 			},
+		// 			// Conditionally include country filter
+		// 			...(country ? [{
+		// 				property_name: "sampcountry",
+		// 				operator: "eq",
+		// 				property_value: country,
+		// 			}] : []),
+		// 			// Conditionally include year filters
+		// 			...(year ? [
+		// 				{
+		// 					property_name: "timestamp",
+		// 					operator: "gte",
+		// 					property_value: `${year}-01-01`,
+		// 				},
+		// 				{
+		// 					property_name: "timestamp",
+		// 					operator: "lte",
+		// 					property_value: `${year}-12-31`,
+		// 				},
+		// 			] : []),
+		// 		],
+		// 	}),
+		// 	plotId: "metrics",
+		// },
 		{
 			type: "data",
 			project: "efsaproject",
@@ -55,6 +55,18 @@ const efsaConfigs = (country, year = null) => {
 						operator: "eq",
 						property_value: country,
 					}] : []),
+					...(year ? [
+						{
+							property_name: "timestamp",
+							operator: "gte",
+							property_value: `${year}-01-01`,
+						},
+						{
+							property_name: "timestamp",
+							operator: "lte",
+							property_value: `${year}-12-31`,
+						},
+					] : []),
 				],
 				order_by: {
 					field: "timestamp",
